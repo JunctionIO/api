3b338cd43841ad7341a317c0de6f785415ce8e92
Mike Georgeff <mike@georgeff.co>
2026-06-20 14:11:03 -0500

    add body parsing middleware to the global stack, add event update route

--- src/AppModule.php
+++ src/AppModule.php
@@ -6,8 +6,11 @@ use Georgeff\Kernel\Environment;
 use Georgeff\Kernel\KernelInterface;
 use Meritum\Http\HttpKernelInterface;
 use Junction\Api\Event\EventSerializer;
+use Junction\Api\Http\Middleware\BodyParser;
+use Junction\Api\Http\Middleware\ValidateUuidId;
 use Junction\Api\Http\Middleware\CreateResource;
 use Junction\Api\Http\Handler\JsonResponseHandler;
+use Junction\Api\Http\Middleware\ValidateContentType;
 use Junction\Api\Http\Middleware\ParsePaginationQuery;
 use Georgeff\Kernel\Module\ConfigurableModuleInterface;
 
@@ -23,6 +26,8 @@ final class AppModule implements ConfigurableModuleInterface
         // Global Middleware
         $kernel->addMiddleware(Http\Middleware\Correlation::class);
         $kernel->addMiddleware(new Http\Middleware\SetRouteArgumentsOnRequest());
+        $kernel->addMiddleware(new ValidateContentType());
+        $kernel->addMiddleware(new BodyParser());
 
         // Routes
         $kernel->addRoute('GET', '/v1/events', JsonResponseHandler::class)
@@ -30,6 +35,12 @@ final class AppModule implements ConfigurableModuleInterface
                ->addMiddleware(Http\Middleware\Event\All::class)
                ->addMiddleware(new CreateResource(new EventSerializer()));
 
+        $kernel->addRoute('PATCH', '/v1/events/{id}', JsonResponseHandler::class)
+               ->addMiddleware(ValidateUuidId::class)
+               ->addMiddleware(Http\Middleware\Event\Exists::class)
+               ->addMiddleware(Http\Middleware\Event\Validator::class)
+               ->addMiddleware(Http\Middleware\Event\Update::class)
+               ->addMiddleware(new CreateResource(new EventSerializer()));
 
         // Kernel Hooks
         $kernel->afterShutdown(new KernelHook\LogDebugInfo());
